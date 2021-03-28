7b0cc90061d03a5fa81d33dce29b73a7a5cef234
Mona <murban@hs-mittweida.de>
2021-03-28 21:27:36 +0200

    add Scripts & Bolt

---
+++ BoltTaskForce/Assets/Scripts/Skill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    public bool isAvailable;
+
+
+    public void Coolown(float duration){
+
+    }
+
+    public void TriggerEffect(){
+
+    }
+}
